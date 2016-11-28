9b4111c20104d794aebb695ab177ba03edfce630
Amaar Fazlani <fazlaniamaar@gmail.com>
2016-11-27 20:26:12 -0600

    Add grid system & style examples & buttons

--- public/bundle.js
+++ public/bundle.js
@@ -24917,11 +24917,14 @@
 	    null,
 	    React.createElement(Nav, null),
 	    React.createElement(
-	      'h2',
-	      null,
-	      ' Main Component '
-	    ),
-	    props.children
+	      'div',
+	      { className: 'row' },
+	      React.createElement(
+	        'div',
+	        { className: 'columns medium-6 large-4 small-centered' },
+	        props.children
+	      )
+	    )
 	  );
 	};
 
@@ -25114,7 +25117,7 @@
 	        React.createElement("input", { type: "text", ref: "location", placeholder: "Enter City Name" }),
 	        React.createElement(
 	          "button",
-	          null,
+	          { className: "button columns small-centered" },
 	          " Get Weather "
 	        )
 	      )
@@ -26694,19 +26697,44 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166),
+	    Link = _require.Link;
+
 	var Examples = function Examples(props) {
 	  return React.createElement(
 	    'div',
 	    null,
 	    React.createElement(
-	      'h3',
-	      null,
+	      'h1',
+	      { className: 'text-center' },
 	      ' Examples'
 	    ),
 	    React.createElement(
 	      'p',
 	      null,
-	      ' Welcome to examples page! '
+	      ' Here are a few example locations to try out: '
+	    ),
+	    React.createElement(
+	      'ol',
+	      null,
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=Houston' },
+	          ' Houston, TX'
+	        )
+	      ),
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=Dallas' },
+	          ' Dallas, TX'
+	        )
+	      )
 	    )
 	  );
 	};
